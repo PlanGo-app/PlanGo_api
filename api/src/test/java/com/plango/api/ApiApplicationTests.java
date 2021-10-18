b4a4fd074a26cf306fa51bc08d96f2a18a0c53d7
Anthony Mendez <anthony.mendez.etu@univ-lille.fr>
2021-10-18 15:15:43 +0200

    init the api project

---
+++ api/src/test/java/com/plango/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plango.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
