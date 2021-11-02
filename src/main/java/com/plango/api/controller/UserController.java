6c685e430dd30913aa3db1fe80646b9e2b60fa60
Annelise Carlier <annelise.carlier.etu@univ-lille.fr>
2021-11-02 23:12:03 +0100

    feat: add put route for users

--- src/main/java/com/plango/api/controller/UserController.java
+++ src/main/java/com/plango/api/controller/UserController.java
@@ -6,7 +6,6 @@ import com.plango.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,8 +17,20 @@ public class UserController {
 
     @PostMapping(path = "", consumes="application/json")
     ResponseEntity<String> createUser(@RequestBody User user) {
-        userService.addUser(user);
+        userService.addOrUpdateUser(user);
         return new ResponseEntity<>(String.format("User %s created", user.getPseudo()), HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{id}")
+    ResponseEntity<String> putUser(@PathVariable Long id,@RequestBody User user) {
+        User userFind = userService.getUserById(id);
+        if(userFind == null){
+            return new ResponseEntity<>(String.format("User %d doesn't exist", id), HttpStatus.BAD_REQUEST);
+        }
+        userFind.setEmail(user.getEmail());
+        userFind.setPassword(user.getPassword());
+        userService.addOrUpdateUser(userFind);
+        return new ResponseEntity<>(String.format("User %d updated", id), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
