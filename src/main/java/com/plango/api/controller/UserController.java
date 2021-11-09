dee4c1535ef0bcfb57c9c9f1861e136c1925c34d
Anthony Mendez <anthony.mendez.etu@univ-lille.fr>
2021-11-09 17:15:39 +0100

    rework on user controller

--- src/main/java/com/plango/api/controller/UserController.java
+++ src/main/java/com/plango/api/controller/UserController.java
@@ -1,18 +1,22 @@
 package com.plango.api.controller;
 
 import com.plango.api.dto.UserDto;
+import com.plango.api.common.exception.UserNotFoundException;
 import com.plango.api.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 public interface UserController {
 
+    @GetMapping(path = "/{id}")
+    User getUserById(@PathVariable Long id) throws UserNotFoundException;
+
     @PostMapping(path = "", consumes="application/json")
     ResponseEntity<String> createUser(@RequestBody User user);
 
     @PutMapping(path = "/{id}")
-    ResponseEntity<String> putUser(@PathVariable Long id, @RequestBody UserDto userDto);
+    ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody UserDto userDto);
 
-    @GetMapping(path = "/{id}")
-    User getUserById(@PathVariable Long id);
+    @DeleteMapping(path = "/delete/{id}")
+    ResponseEntity<String> deleteUser(@PathVariable Long id);
 }
--- src/main/java/com/plango/api/service/UserService.java
+++ src/main/java/com/plango/api/service/UserService.java
@@ -1,11 +1,13 @@
 package com.plango.api.service;
 
+import com.plango.api.common.exception.UserNotFoundException;
 import com.plango.api.entity.User;
 
 public interface UserService {
-    void addUser(User user) throws Exception;
-    void updateUser(User user);
-    User getUserById(Long id);
+    void createUser(User user);
+    void updateUser(User user) throws UserNotFoundException;
+    User getUserById(Long id) throws UserNotFoundException;
+    void deleteUser(Long id) throws UserNotFoundException;
     boolean pseudoTaken(String pseudo);
     boolean emailTaken(String email);
 }
