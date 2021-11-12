0adaf691dc5bfcd7656607b0a370954ff8b91d3d
Anthony Mendez <anthony.mendez.etu@univ-lille.fr>
2021-11-12 16:02:10 +0100

    replace composite key for user by setting columns unique to true

--- src/main/java/com/plango/api/entity/UserCompositeKey.java
+++
@@ -1,9 +0,0 @@
-package com.plango.api.entity;
-
-import java.io.Serializable;
-
-public class UserCompositeKey implements Serializable {
-    private Long id;
-    private String email;
-    private String pseudo;
-}
