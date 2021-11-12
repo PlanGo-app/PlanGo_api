21932548576d533de9f4e111f2e3616449b5f431
Anthony Mendez <anthony.mendez.etu@univ-lille.fr>
2021-11-12 16:49:20 +0100

    upgrading security by using dto instead of entities

--- src/main/java/com/plango/api/controller/UserController.java
+++ src/main/java/com/plango/api/controller/UserController.java
@@ -6,6 +6,7 @@ import com.plango.api.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/user")
 public interface UserController {
 
     @GetMapping(path = "/{id}")
