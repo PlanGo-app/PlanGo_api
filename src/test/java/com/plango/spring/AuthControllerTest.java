ce28ed9a38cd5848d563519361c225c205c5aecd
Carlier Annelise <carliera@b11p6.fil.univ-lille1.fr>
2021-11-16 15:35:49 +0100

    test: authController login and signup

--- src/test/java/com/plango/spring/AuthControllerTest.java
+++ src/test/java/com/plango/spring/AuthControllerTest.java
@@ -1,11 +1,15 @@
 package com.plango.spring;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import com.plango.api.controller.AuthController;
 import com.plango.api.controller.UserController;
 import com.plango.api.controller.impl.AuthControllerImpl;
 import com.plango.api.dto.CredentialDto;
+import com.plango.api.entity.User;
 import com.plango.api.security.JwtGenerator;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,6 +20,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 
 public class AuthControllerTest {
 
@@ -31,18 +39,54 @@ public class AuthControllerTest {
     @Mock
     private JwtGenerator jwtGenerator;
 
+    private CredentialDto credentials;
+    private User newUser;
+
+    final String TEST = "test";
+
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
+        credentials = new CredentialDto();
+        credentials.setUsername(TEST);
+        credentials.setPassword(TEST);
+        newUser = new User();
+        newUser.setEmail("test@gmail.com");
+        newUser.setPseudo(TEST);
+        newUser.setPassword(TEST);
     }
 
     @Test
-    public void testLoginWhenUserDontExists() {
-        CredentialDto credentials = new CredentialDto();
-        credentials.setUsername("test");
-        credentials.setPassword("test");
+    public void testLoginWhenBadCredentials() {
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenThrow(new BadCredentialsException(anyString()));
+
         ResponseEntity<String> response = authController.login(credentials);
-        assertEquals("Wrong pseudo or password.", response.getBody());
+
+        verify(authenticationManager).authenticate(any(UsernamePasswordAuthenticationToken.class));
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    public void testLoginWithValidUser() {
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(new TestingAuthenticationToken(credentials, credentials.getPassword()));
+        when(jwtGenerator.generateToken(any(Authentication.class))).thenReturn("token");
+
+        ResponseEntity<String> response = authController.login(credentials);
+
+        verify(jwtGenerator).generateToken(any(Authentication.class));
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void userAlreadyExistsWhenSignUp() {
+        when(userController.createUser(newUser)).thenReturn(new ResponseEntity<String>(anyString(), HttpStatus.BAD_REQUEST));
+
+        ResponseEntity<String> response = authController.signup(newUser);
+
+        verify(userController).createUser(newUser);
+
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
     }
 }
