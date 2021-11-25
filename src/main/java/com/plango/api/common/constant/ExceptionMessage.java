017dd5ea5afd41bcfeeee11fc59c01d043082110
Anthony Mendez <anthony.mendez.etu@univ-lille.fr>
2021-11-25 19:11:14 +0100

    feat: Implement PlanningEvent controller and service

--- src/main/java/com/plango/api/common/constant/ExceptionMessage.java
+++ src/main/java/com/plango/api/common/constant/ExceptionMessage.java
@@ -2,7 +2,7 @@ package com.plango.api.common.constant;
 
 public final class ExceptionMessage {
     private ExceptionMessage() {}
-    
+
     public static final String CURRENT_USER_NOT_FOUND = "Current user not found";
     public static final String CURRENT_USER_NOT_ALLOWED_TO_GET_PLANNING_EVENT = "Current user not allowed to get requested planning event";
     public static final String CURRENT_USER_NOT_ALLOWED_TO_UPDATE_PLANNING_EVENT = "Current user not allowed to update requested planning event";
