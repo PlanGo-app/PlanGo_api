c5ea8d863e202fbf6891bbc7a21b3895144054b4
Anthony Mendez <anthony.mendez.etu@univ-lille.fr>
2021-12-30 23:09:04 +0100

    feat: add @CrossOrigin

--- src/main/java/com/plango/api/controller/AuthController.java
+++ src/main/java/com/plango/api/controller/AuthController.java
@@ -7,10 +7,12 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin
 @RequestMapping("/auth")
 public interface AuthController {
 
